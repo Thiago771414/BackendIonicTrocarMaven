5ff62f8cd8144420f96892157fea46f93bd94b56
Thiago Lima <thiso2@hotmail.com>
2020-06-11 17:35:50 -0300

    Endpoint para buscar cliente por email

---
+++ src/main/java/com/thiagolima/cursomc/services/PedidoService.java
@@ -0,0 +1,91 @@
+package com.thiagolima.cursomc.services;
+
+import java.util.Date;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.thiagolima.cursomc.domain.Cliente;
+import com.thiagolima.cursomc.domain.ItemPedido;
+import com.thiagolima.cursomc.domain.PagamentoComBoleto;
+import com.thiagolima.cursomc.domain.Pedido;
+import com.thiagolima.cursomc.domain.enums.EstadoPagamento;
+import com.thiagolima.cursomc.repositories.ItemPedidoRepository;
+import com.thiagolima.cursomc.repositories.PagamentoRepository;
+import com.thiagolima.cursomc.repositories.PedidoRepository;
+import com.thiagolima.cursomc.security.UserSS;
+import com.thiagolima.cursomc.services.exceptions.AuthorizationException;
+
+import javassist.tools.rmi.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+	
+	@Autowired
+	private ClienteService clienteService;
+	
+	@Autowired
+	private EmailService emailService;
+	
+	public Pedido find(Integer id) throws ObjectNotFoundException {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+	
+	@Transactional
+	public Pedido insert(Pedido obj) throws ObjectNotFoundException{
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+        obj.getPagamento().setPedido(obj);
+        if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preecherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+        obj = repo.save(obj);
+        pagamentoRepository.save(obj.getPagamento());
+        for (ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
+			ip.setPedido(obj);
+		}
+        itemPedidoRepository.saveAll(obj.getItens());
+        emailService.sendOrderConfirmationHtmlEmail(obj);
+        System.out.println(obj);
+        return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) throws ObjectNotFoundException {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
+	}
+}
